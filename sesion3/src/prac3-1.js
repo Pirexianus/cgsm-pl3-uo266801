4c460d092c66b1108c924c404624d9d065235b66
Miguel <uo266801@uniovi.es>
2024-02-08 20:14:42 +0100

    prac3-3

---
+++ sesion3/src/prac3-1.js
@@ -0,0 +1,63 @@
+import * as THREE from 'three';
+
+import WEBGL from 'three/examples/jsm/capabilities/WebGL.js';
+
+if ( WEBGL.isWebGLAvailable() ) 
+{
+    //alert("WebGL is working");
+} 
+else 
+{
+    alert("WebGL error: " + WEBGL.getWebGLErrorMessage());
+}
+
+const scene = new THREE.Scene();
+
+const renderer = new THREE.WebGLRenderer( {antialias: true} );
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+window.addEventListener( 'resize', ( ) => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix( );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.render( scene, camera );
+}, false );
+
+const camera = new THREE.PerspectiveCamera ( 45, window.innerWidth / window.innerHeight, 1, 4000 );
+camera.position.set( 0, 0, 300 );
+
+const geometry = new THREE.BoxGeometry( 100, 100, 100 );
+const textureLoader = new THREE.TextureLoader( );  // The object used to load textures
+const material = new THREE.MeshPhongMaterial(
+    {
+        map: textureLoader.load( "textures/brick.jpg" ),
+        bumpMap: textureLoader.load( "textures/brick-map.jpg" )
+    } );
+const box = new THREE.Mesh( geometry, material );
+box.rotation.set( Math.PI / 5, Math.PI / 5, 0 );
+
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 1.0 );
+directionalLight.position.set( 0, 400, 400 );
+
+scene.add( box );
+scene.add( directionalLight );
+const clock = new THREE.Clock( );
+
+function animate( ) {
+
+    const delta = clock.getDelta( ); // Elapsed time in seconds
+
+    // UPDATE THE SCENE ACCORDING TO THE ELAPSED TIME
+    const Rotation = 0.01;
+    box.rotation.y+=Rotation;
+
+    // Render the scene
+    renderer.render( scene, camera );
+
+    // Request the browser to execute the animation-rendering loop
+    requestAnimationFrame( animate );
+};
+animate(); 
+
+//renderer.render( scene, camera );
