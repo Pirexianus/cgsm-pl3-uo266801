2566637b6380844b870a23debf180d3e3426c466
Miguel <uo266801@uniovi.es>
2024-02-15 20:22:11 +0100

    Prac3-3

--- sesion4/webpack.config.js
+++ sesion4/webpack.config.js
@@ -3,7 +3,8 @@ module.exports = {
     entry: {
         "prac4-1": "./src/prac4-1.js",
         "prac4-2": "./src/prac4-2.js",
-        "prac4-3": "./src/prac4-3.js"
+        "prac4-3": "./src/prac4-3.js",
+        "prac4-4": "./src/prac4-4.js"
     },
     devtool: 'inline-source-map',
     devServer: {
