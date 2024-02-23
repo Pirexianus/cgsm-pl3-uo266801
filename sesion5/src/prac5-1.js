b979a350cff233ffc5715dac4ac10d92546371d8
Miguel <uo266801@uniovi.es>
2024-02-23 19:19:54 +0100

    I want to die

--- sesion5/src/prac5-1.js
+++ sesion5/src/prac5-1.js
@@ -1,5 +1,17 @@
-import dash.all.min.js;
+// Importa la librería Dash.js desde el archivo separado
+const script = document.createElement('script');
+script.src = 'C:/Users/uo266801/Documents/cgsm-pl3-uo266801/dash.all.min.js';
+script.onload = function() {
+    // Una vez que la librería Dash.js se haya cargado correctamente, puedes utilizarla
+    document.addEventListener("DOMContentLoaded", function() {
+        // URL del manifiesto DASH
+        const url = "http://localhost:60080/sintel480fix.mpd";
 
-const url = "url_al_manifiesto";
-const player = dashjs.MediaPlayer().create();
-player.initialize(document.querySelector("#player"), url, true);
+        // Crea un reproductor Dash.js
+        const player = dashjs.MediaPlayer().create();
+
+        // Inicializa el reproductor con el manifiesto DASH
+        player.initialize(document.querySelector("#player"), url, true);
+    });
+};
+document.head.appendChild(script);
