2be226e4a0d6e086266fc704f2cb760069b64e5b
Miguel <uo266801@uniovi.es>
2024-02-29 20:00:40 +0100

    Begin prac6-4

---
+++ sesion6/src/prac6-4.js
@@ -0,0 +1,76 @@
+import * as THREE from 'three';
+
+import WEBGL from 'three/examples/jsm/capabilities/WebGL.js';
+
+if ( WEBGL.isWebGLAvailable() ) 
+{
+    //alert("WebGL is working");
+} 
+else 
+{
+    alert("WebGL error: " + WEBGL.getWebGLErrorMessage());
+}
+
+const scene = new THREE.Scene();
+
+const renderer = new THREE.WebGLRenderer( {antialias: true} );
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+window.addEventListener( 'resize', ( ) => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix( );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.render( scene, camera );
+}, false );
+const camera = new THREE.PerspectiveCamera ( 45, window.innerWidth / window.innerHeight, 1, 4000 );
+camera.position.set( 0, 0, 300 );
+
+const video = document.getElementById( 'video' );
+const image = document.createElement( 'canvas' );
+image.width = 480;  // Video width
+image.height = 204; // Video height
+const imageContext = image.getContext( '2d' );
+imageContext.fillStyle = '#000000';
+imageContext.fillRect( 0, 0, image.width - 1, image.height - 1 );
+const texture = new THREE.Texture( image );
+const material = new THREE.MeshBasicMaterial( { map: texture } );
+const materiall = new THREE.MeshBasicMaterial();
+const wall = new THREE.Mesh( new THREE.PlaneGeometry( image.width, image.height, 4, 4 ), material );
+
+const light = new THREE.AmbientLight();
+light.position.set(200,0,0);
+
+scene.add(wall);
+scene.add(light);
+const clock = new THREE.Clock( );
+
+function animate( ) {
+
+    const delta = clock.getDelta( ); // Elapsed time in seconds
+
+    // UPDATE THE SCENE ACCORDING TO THE ELAPSED TIME
+    if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
+
+        imageContext.drawImage( video, 0, 0 );
+        if ( texture ) texture.needsUpdate = true;
+    }
+    wall.rotation.y+=0.01;
+    // Render the scene
+    renderer.render( scene, camera );
+
+    // Request the browser to execute the animation-rendering loop
+    requestAnimationFrame( animate );
+};
+// Stream webcam footage to the video element
+navigator.mediaDevices.getUserMedia({ video: true })
+.then((stream) => {
+    video.srcObject = stream;
+    video.onloadedmetadata = (e) => {
+        video.play();
+    };
+})
+.catch((e) => { console.log('getUserMedia() error: ' + e.name); });
+
+animate(); 
+
+//renderer.render( scene, camera );
